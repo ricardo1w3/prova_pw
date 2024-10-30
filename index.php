0a98a7a073b41c33805d20e68d3130f011305ec6
Ricardo Souza <ricardo03.santos.2021@gmail.com>
2024-10-30 09:31:18 -0300

    Add files via upload
    infelizmente somos incapazes

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade de PW</title>
+</head>
+<body>
+    <h1> João Ramos e Ricardo</h1>
+    <img src="c:\Users\Aluno\Downloads\pizza" alt="Clique no botão para receber" >
+    <a href="cadastro.php" class="botao">Ir para Cadastro</a>
+</body>
+</html>
